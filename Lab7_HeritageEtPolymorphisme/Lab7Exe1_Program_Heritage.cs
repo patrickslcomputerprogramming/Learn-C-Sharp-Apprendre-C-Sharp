4e643c467418b7ecff0c5b7592f426d803ca1a98
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2025-07-29 19:45:46 -0400

    Add files via upload

---
+++ Lab7_HeritageEtPolymorphisme/Lab7Exe1_Program_Heritage.cs
@@ -0,0 +1,79 @@
+﻿// Program.cs
+
+// Importer les espaces de nom (namespace)
+using System;
+using System.Globalization;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+//A-Créez une « classe » de base nommée Cercle qui permet de créer des objets correspondants à des cercles de tailles variées.
+class Cercle
+{
+    //Champ
+    private double rayon;
+
+    //1.Une « méthode constructeur » qui reçoit un paramètre nommé « rayon » dont la valeur est affectée à un champ nommé « rayon ».
+    public Cercle (double rayon)
+    {
+        this.rayon = rayon;
+    }
+
+    //2.Une méthode nommée « Surface » qui calcule et renvoie la surface à partir du rayon(surface = Math.PI * Rayon * Rayon).
+    public double Surface ()
+    {
+        return Math.PI * this.rayon * this.rayon;
+    }
+}
+
+//B - Créez une « classe » dérivée de la classe Cercle nommée « Cylindre » qui contient :
+sealed class Cylindre : Cercle
+{
+    //Champs
+    private double hauteur;
+
+    //1.Une « méthode constructeur » qui reçoit le paramètre nommé « rayon » de la méthode constructeur de la classe de base; et un deuxième paramètre nommé « hauteur » dont la valeur est affectée à un champ nommé « hauteur ».
+    public Cylindre(double rayon, double hauteur) : base(rayon)
+    {
+        this.hauteur = hauteur;
+    }
+
+    //2.Une méthode nommée « Volume » qui calcule et renvoie le volume à partir de la surface et de la hauteur (volume = surface * hauteur).
+    public double Volume()
+    {
+        return this.Surface() * this.hauteur;
+    }
+}
+
+// Déclarer la classe contenant la méthode Main
+class ProgrammePrincipal
+{
+    // Déclarer la méthode Main: point d'entrée
+    static void Main()
+    {
+        //D-Créez un objet pour tester la classe Cercle. Utilisez les données de test ci-dessous :  
+	    //Entrées: rayon = 5 - Sorties : surface = 78.54
+        double rayon_cercle = 5;
+        Cercle cercle = new Cercle(rayon_cercle);
+        //Appeler des methodes avec un objet
+        double surface_cercle = cercle.Surface();
+        //Afficher
+        Console.WriteLine("Classe Cercle");
+        Console.WriteLine("Rayon Cercle = " + rayon_cercle.ToString("0.00"));
+        Console.WriteLine("Surface Cercle = " + surface_cercle.ToString("0.00"));
+
+        //E-Créez un objet pour tester la classe Cylindre. Utilisez les données de test ci-dessous :  
+	    //Entrées: rayon = 5   hauteur = 7 - Sorties : surface = 78.54   volume = 549.78
+        double hauteur_cylindre = 7;
+        Cylindre cylindre = new Cylindre(rayon_cercle, hauteur_cylindre);
+        //Appeler des methodes avec un objet
+        double surface_cylindre = cylindre.Surface();
+        double volume_cylindre = cylindre.Volume();
+        //Afficher
+        Console.WriteLine();
+        Console.WriteLine("Classe Cylindre");
+        Console.WriteLine("Rayon Cylindre = " + rayon_cercle.ToString("0.00"));
+        Console.WriteLine("Surface Cylindre = " + surface_cylindre.ToString("0.00"));
+        Console.WriteLine("Volume Cylindre = " + volume_cylindre.ToString("0.00"));
+    }
+}
+
---
+++ Lab7_HeritageEtPolymorphisme/Lab7exe2_Program_Polymorphisme.cs
@@ -0,0 +1,82 @@
+﻿//Program.cs
+
+//Importer les espaces de nom (namespace)
+using System;
+using System.Globalization;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+//A-Créez une « classe » de base nommée Fruits qui permet de créer des objets correspondants à des fruits. 
+class Fruits
+{
+    //Champ
+    protected string nom_fruit;
+
+    //1.Une « méthode constructeur » qui reçoit un paramètre nommé « nom_fruit » dont la valeur est affectée à un champ nommé « nom_fruit ».
+    public Fruits (string nom_fruit)
+    {
+        this.nom_fruit = nom_fruit;
+    }
+
+    //2.Une « méthode » virtuelle qui retourne la couleur d’un fruit. 
+    public virtual string CouleurFruit()
+    {
+        return "Affiche la couleur du fruit!";
+    }
+}
+
+//B-Créez une « classe » dérivée de la classe Fruit nommée « Pomme » 
+sealed class Pomme : Fruits
+{
+    //Champs
+    private string couleur_fruit = "rouge";
+
+    //Méthode constructeur  
+    public Pomme(string nom_fruit) : base(nom_fruit) { }
+
+    //1.Une « méthode » qui redéfinit la méthode virtuelle de la classe de base et retourne la couleur de la pomme.
+    public override string CouleurFruit()
+    {
+        return this.couleur_fruit;
+    }
+}
+
+//C-Créez une « classe » dérivée de la classe Fruit nommée « Melon » qui contient :
+class Melon : Fruits
+{
+    //Champs
+    private string couleur_fruit = "jaune" ;
+
+    //Méthode constructeur 
+    public Melon (string nom_fruit) : base(nom_fruit) { }
+
+    //1.Une « méthode » qui redéfinit la méthode virtuelle de la classe de base et retourne la couleur de la pomme.
+    public override string CouleurFruit()
+    {
+        return this.couleur_fruit;
+    }
+
+}
+
+// Déclarer la classe contenant la méthode Main
+class ProgrammePrincipal
+{
+    // Déclarer la méthode Main: point d'entrée
+    static void Main()
+    {
+        //Démontrer que le polymorphisme permet à des objets de classes différentes d'être traités comme des objets d'une même classe de base en créant un objet pour tester les classes, suivant la notation polymorphique telle que montrée ci - dessous.
+        //Utilisation polymorphique
+        Fruits[] fruits = { 
+            new Pomme("Pomme Cannelle"), 
+            new Melon("Melon d’eau") 
+        };
+
+        //Utiliser une boucle foreach pour appeler le champ « nom_fruit » et la méthode virtuelle redéfinie dans les classes dérivées.
+        foreach (var chaqueFruit in fruits) { 
+            Console.WriteLine("Nom fruit : " + chaqueFruit);
+            Console.WriteLine("Couleur fruit : " + chaqueFruit.CouleurFruit());
+            Console.WriteLine();
+        }
+    }
+}
+
